990ce8d4f89cc30f5d515f6fb37191337879b267
Kat0 <chaaulong@gmail.com>
2020-12-24 01:40:52 +0800

    Update login.js

--- login.js
+++ login.js
@@ -11,13 +11,17 @@ router.get('/login', (req,res) => {
 router.post('/login', (req,res) => {
 
 	users.forEach((user) => {
+		let valid = false;
 		if (user.name == req.body.username && user.password == req.body.password) {
 
 			req.session.authenticated = true;
 			req.session.username = req.body.username;
+			valid = true;
 
-		}else{
-			res.redirect('/register');}
+		}
+		if (valid){
+			res.redirect('/register');
+		}
 	});
 	res.redirect('/');
 });
